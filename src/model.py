f5c1ce732fa7e3cb8a6122b17d78587f66296fa6
Trckmazz <dylanprinsloo@icloud.com>
2024-11-04 09:10:40 +0200

    unet_1728190905

--- src/model.py
+++ src/model.py
@@ -1,1 +1,36 @@
 # model architecture
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate
+from tensorflow.keras.models import Model
+
+def unet_vgg16_model(input_shape):
+    vgg_base = VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
+    for layer in vgg_base.layers:
+        layer.trainable = False
+
+    inputs = vgg_base.input
+    c1 = vgg_base.get_layer('block1_conv2').output
+    p1 = MaxPooling2D((2, 2))(c1)
+    c2 = vgg_base.get_layer('block2_conv2').output
+    p2 = MaxPooling2D((2, 2))(c2)
+    c3 = vgg_base.get_layer('block3_conv3').output
+    c4 = vgg_base.get_layer('block4_conv3').output
+
+    u5 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(c4)
+    u5 = concatenate([u5, c3])
+    c5 = Conv2D(256, (3, 3), activation='relu', padding='same')(u5)
+    c5 = Conv2D(256, (3, 3), activation='relu', padding='same')(c5)
+
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c2])
+    c6 = Conv2D(128, (3, 3), activation='relu', padding='same')(u6)
+    c6 = Conv2D(128, (3, 3), activation='relu', padding='same')(c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c1])
+    c7 = Conv2D(64, (3, 3), activation='relu', padding='same')(u7)
+    c7 = Conv2D(64, (3, 3), activation='relu', padding='same')(c7)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c7)
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
